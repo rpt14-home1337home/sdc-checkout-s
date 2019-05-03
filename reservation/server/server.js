0168d45d627478c3638ba72bef387b6976cc596c
David Nguyen <tnguyen.david@gmail.com>
2019-05-03 14:09:18 -0700

    Added middleware to log all requests received

--- reservation/server/server.js
+++ reservation/server/server.js
@@ -4,9 +4,18 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3002;
 
+// Log all 4xx and 5xx responses
 app.use(morgan('dev'));
+
+// Log all requests to access.log
+app.use(morgan('common', {
+  stream: fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+}));
+
+// Serve public folder
 app.use(express.static(path.join(__dirname, '../public')));
 
+// Listen for requests
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
 });
