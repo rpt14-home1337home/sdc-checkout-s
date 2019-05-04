cc7a663e9767a2ef20a2bad9020166e180b9ca87
David Nguyen <tnguyen.david@gmail.com>
2019-05-03 20:10:23 -0700

    Complete footer placeholder

--- reservation/client/components/reservation.jsx
+++ reservation/client/components/reservation.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 const Reservation = () => (
-  <h1>Hello World</h1>
+  <div id="footer">
+    <div className="home-plus" id="logo">
+      <svg className="home-plus"></svg>
+    </div>
+    <div id="name">
+      <div className="home-name">Entire Loft In Cape Town</div>
+      <button className="reviews">
+        <div>
+          <span className="star-rating"></span>
+          <span className="reviews-count">167</span>
+        </div>
+      </button>
+    </div>
+    <div id="price">
+      <span className="price">$57 </span>
+      <span className="duration">/ night</span>
+    </div>
+    <div id="book">
+      <button className="book">Book</button>
+    </div>
+  </div>
 );
 
 export default Reservation;
