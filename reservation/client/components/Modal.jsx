21069030cbfc8574b23d025774bf46482c678fc8
David Nguyen <tnguyen.david@gmail.com>
2019-05-05 00:02:16 -0700

    Moved modal inline style to css module

--- reservation/client/components/Modal.jsx
+++ reservation/client/components/Modal.jsx
@@ -13,53 +13,13 @@ class Modal extends React.Component {
   }
 
   render() {
-    const backdropStyle = {
-      position: 'fixed',
-      top: 0,
-      right: 0,
-      bottom: 0,
-      left: 0,
-      backgroundColor: 'rgba(255, 255, 255, 0.8)',
-      zIndex: 1000
-    };
-
-    const modalContainer = {
-      display: 'table',
-      height: '100%',
-      width: '100%'
-    };
-
-    const modalCell = {
-      display: 'table-cell',
-      verticalAlign: 'middle',
-      padding: '64px'
-    };
-
-    const modalStyle = {
-      width: '100%',
-      position: 'relative',
-      margin: 'auto',
-      backgroundColor: 'rgb(255, 255, 255)',
-      boxShadow: 'rgba(0, 0, 0, 0.2) 0px 1px 10px 0px',
-      maxWidth: '376px'
-    };
-
-    const modal = {
-      backgroundColor: 'rgb(255, 255, 255)',
-      padding: '24px',
-    };
-
-    const closeButtonStyle = {
-      marginBottom: '24px'
-    };
-
     return (
-      <div style={backdropStyle}>
-        <div style={modalContainer}>
-          <div style={modalCell}>
-            <div style={modalStyle}>
-              <section style={modal}>
-                <div style={closeButtonStyle}>
+      <div id="modal-backdrop">
+        <div id="modal-container">
+          <div id="modal-cell">
+            <div id="modal-content">
+              <section id="modal-spacing">
+                <div id="modal-button">
                   <button
                     type="button"
                     id="button-close"
