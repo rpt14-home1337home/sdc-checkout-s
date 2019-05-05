57e1ab624b768c88cda9d717c66c9a7cd547d48a
David Nguyen <tnguyen.david@gmail.com>
2019-05-04 17:07:08 -0700

    Added state to price component

--- reservation/client/components/Price.jsx
+++ reservation/client/components/Price.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
 
-const Price = () => (
-  <div id="price">
-    <div id="price-spacing">
-      <div id="price-info">
-        <span className="price">$57 </span>
-        <span className="duration">/ night</span>
+class Price extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      price: '57'
+    };
+  }
+
+  getPrice() {
+    //ajax call
+  }
+
+  render() {
+    return (
+      <div id="price">
+        <div id="price-spacing">
+          <div id="price-info">
+            <span className="price">${this.state.price} </span>
+            <span className="duration">/ night</span>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export default Price;
