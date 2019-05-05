351d739174403199a513915b75532037222f5d19
David Nguyen <tnguyen.david@gmail.com>
2019-05-04 17:11:03 -0700

    Added state to property component

--- reservation/client/components/Property.jsx
+++ reservation/client/components/Property.jsx
@@ -1,23 +1,43 @@
 import React from 'react';
 
-const Property = () => (
-  <div id="property">
-    <div id="property-spacing">
-      <div id="property-name">Entire Loft In Cape Town</div>
-      <button id="reviews">
-        <div>
-          <span id="star-ratings-spacing">
-            <span role="img" id="star-ratings">
-              <span id="property-star-ratings"></span>
-            </span>
-          </span>
-          <span id="reviews-spacing">
-            <span id="reviews-count">166</span>
-          </span>
+class Property extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'Entire Loft In Cape Town',
+      reviews: 166
+    };
+  }
+
+  getName() {
+    //ajax call
+  }
+
+  getNumberOfReviews() {
+    //ajax call
+  }
+
+  render() {
+    return (
+      <div id="property">
+        <div id="property-spacing">
+          <div id="property-name">{this.state.name}</div>
+          <button id="reviews">
+            <div>
+              <span id="star-ratings-spacing">
+                <span role="img" id="star-ratings">
+                  <span id="property-star-ratings"></span>
+                </span>
+              </span>
+              <span id="reviews-spacing">
+                <span id="reviews-count">{this.state.reviews}</span>
+              </span>
+            </div>
+          </button>
         </div>
-      </button>
-    </div>
-  </div>
-);
+      </div>
+    );
+  }
+}
 
 export default Property;
