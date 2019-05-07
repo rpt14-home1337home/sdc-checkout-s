e54333770d470c387b46e291b3877f9d4713a627
David Nguyen <tnguyen.david@gmail.com>
2019-05-06 21:29:04 -0700

    Complete custom date picker

--- reservation/client/components/Modal.jsx
+++ reservation/client/components/Modal.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import Price from './Price.jsx';
 import Ratings from './Ratings.jsx';
+import AirbnbButton from './AirbnbButton.jsx';
+import 'react-dates/initialize';
+import { DateRangePicker } from 'react-dates';
 
 class Modal extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      startDate: null,
+      endDate: null,
+      focusedInput: null
+    };
     this.handleClose = this.handleClose.bind(this);
   }
 
@@ -30,11 +38,44 @@ class Modal extends React.Component {
                 </div>
                 <section>
                   <div>
-                    <Price />
-                    <Ratings />
-                    <div style={{marginTop: "16px", marginBottom: "16px"}}>
-                      <div id="modal-divider"></div>
-                    </div>
+                    <section>
+                      <Price />
+                      <Ratings />
+                      <div style={{marginTop: "16px", marginBottom: "16px"}}>
+                        <div id="modal-divider"></div>
+                      </div>
+                    </section>
+                    <form>
+                      <div id="book-fields-placeholder">
+                        <div id="modal-book-spacing">
+                          <label id="book-dates">Dates</label>
+                          {/* <div id="book-it-table">
+                            <div id="book-it-row">
+
+                            </div>
+                          </div> */}
+                        </div>
+                        <DateRangePicker
+                          startDate={this.state.startDate}
+                          startDateId='startDate'
+                          endDate={this.state.endDate}
+                          endDateId='endDate'
+                          onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })}
+                          focusedInput={this.state.focusedInput}
+                          onFocusChange={focusedInput => this.setState({ focusedInput })}
+                          startDatePlaceholderText='Check-in'
+                          endDatePlaceholderText='Checkout'
+                          numberOfMonths={1}
+                          verticalSpacing={12}
+                          block={true}
+                          showClearDates={true}
+                        />
+                      </div>
+                      <div id="temp-margin-button">
+                        <AirbnbButton name="Book"/>
+                      </div>
+                      <div id="charge">You won't be charged yet</div>
+                    </form>
                   </div>
                 </section>
               </section>
