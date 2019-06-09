c7b3387a5c52f44169e83509c84d583db47eca7f
David Nguyen <tnguyen.david@gmail.com>
2019-06-08 17:20:41 -0700

    Added tests for rendering button

--- reservation/test/booking.test.js
+++ reservation/test/booking.test.js
@@ -1,8 +1,18 @@
 import Booking from '../client/components/Booking.jsx';
+import Button from '../client/components/AirbnbButton.jsx';
+
+let wrapper;
+
+beforeEach(() => {
+  wrapper = shallow(<Booking /> );
+});
 
 describe('<Booking /> rendering', () => {
-  it('should render one <h1>', () => {
-      let wrapper = shallow(<Booking />);
-      expect(wrapper.children('div')).toHaveLength(1);
+  it('should render one <div>', () => {
+    expect(wrapper.children('div')).toHaveLength(1);
+  });
+
+  it('should render one <Button>', () => {
+    expect(wrapper.find(Button)).toHaveLength(1);
   });
 });
