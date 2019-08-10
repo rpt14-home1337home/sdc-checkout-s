b176539f4a72c4c01920955221e74954e81b7b0d
Manuel Duran <manuelduran@Manuels-MacBook-Air.local>
2019-08-10 12:00:41 -0500

    added several delete functions to db

--- server/db/checkout.js
+++ server/db/checkout.js
@@ -17,5 +17,35 @@ const insertRecord = (checkout, cb) => {
   });
 };
 
+const deleteRecord = (id, cb) => {
+  db.queryAsync(`DELETE from airbnb where id = ?`), id, (err, results) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(`Deleted record ${JSON.stringify(id)}`);
+  }
+}
+
+const deleteRecordsBatch = (startId, endId, cb) => {
+  db.queryAsync('DELETE from airbnb where id >= ? && id <= ?'), [startId, endId], (err, results) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(`Deleted records ${JSON.stringify(startId)} to ${JSON.stringify(endId)}`);
+  }
+}
+
+const deleteRecordsAll = (cb) => {
+  //`TRUNCATE airbnb`
+  db.queryAsync('TRUNCATE airbnb', (err, results) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(`Deleted all records in airbnb`);
+  })
+}
 module.exports.insertRecord = insertRecord;
 module.exports.getRecords = getRecords;
+module.exports.deleteRecord = deleteRecord;
+module.exports.deleteRecordsBatch = deleteRecordsBatch;
+module.exports.deleteRecordsAll = deleteRecordsAll;
