0d47372af838db96d7725f468657ad37f0c2c66d
Manuel Duran <manuelduran@Manuels-MacBook-Air.local>
2019-08-16 10:41:49 -0500

    created csv file with 10M records

--- server/db/seedTenMillion.js
+++
@@ -1,7 +0,0 @@
-const faker = require('faker');
-const fs = require('fs');
-const promise = require('bluebird');
-const fsAsync = promise.promisifyAll(fs);
-const path = require('path');
-const output = path.join(__dirname, '/seedTenMillion.sql');
-
