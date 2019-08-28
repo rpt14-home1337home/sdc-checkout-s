e94b854886889dc7aa3db912d506905b63d617cf
Manuel Duran <manuelduran@Manuels-MacBook-Air.local>
2019-08-28 09:54:27 -0500

    refactored seeding script to work with new schema

--- server/db_pg/models/checkout.js
+++ server/db_pg/models/checkout.js
@@ -2,7 +2,7 @@ module.exports = () => {
   return (
     `CREATE TABLE IF NOT EXISTS checkout (
       id              SERIAL PRIMARY KEY,
-      prop_id       INTEGER,
+      propId       INTEGER,
       checkin       DATE NOT NULL,
       checkout        DATE NOT NULL
     );`
