bd7086ef3964daefa0b1c3d7911fc3aa7bd0537e
Manuel Duran <manuelduran@Manuels-MacBook-Air.local>
2019-08-29 19:47:18 -0500

    Changed database I'm connecting to

--- server/db_pg/index.js
+++ server/db_pg/index.js
@@ -1,11 +1,10 @@
 const promise = require('bluebird');
 const initOpts = { promiseLib: promise };
 const pgp = require('pg-promise')(initOpts);
-const database = 'airbnb';
 const connection = {
   host: process.env.PGHOST || 'localhost',
   port: process.env.PGPORT || '5432',
-  database: 'airbnb_test',
+  database: process.env.PGDATABASE || 'airbnb',
   user: process.env.PGUSER || 'postgres',
   password: process.env.PGPASSWORD
 };
