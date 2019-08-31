65ba702e4e40094aa0508c76bc938f81fa59fdb7
Manuel Duran <manuelduran@Manuels-MacBook-Air.local>
2019-08-31 11:59:50 -0500

    refactored insert controller

--- server/db_pg/controllers/index.js
+++ server/db_pg/controllers/index.js
@@ -1,29 +1,17 @@
 const db = require('../index.js');
 
 let insertRecord = (data, cb) => {
-  //check if records exist for date range
-  getRecordsByDate(data, (err, records) => {
-    if (err) {
-      cb(err);
-    } else if (records.length > 0) {
-      err = new Error('Invalid date range: records exist within interval provided. ' + JSON.stringify(records) );
-      cb(err);
-    } else {
-      //if no records exist, insert new record
-      data = [data.id, data.checkin, data.checkout];
-      var query = `INSERT INTO checkout(prop_id, checkin, checkout) VALUES ($1, $2, $3)`;
-      db.any(query, data)
-        .then((res) => {
-          console.log(`Inserted ${data} into table`);
-          cb(null, res);
-        })
-        .catch((e) => {
-          console.log(e);
-          cb(e);
-        })
-    }
-
-  })
+  data = [data.id, data.checkin, data.checkout];
+  var query = `INSERT INTO checkout(propid, checkin, checkout) VALUES ($1, $2, $3)`;
+  db.any(query, data)
+    .then((res) => {
+      console.log(`Inserted ${data} into table`);
+      cb(null, res);
+    })
+    .catch((e) => {
+      console.log(e);
+      cb(e);
+    })
 }
 
 let getRecordsByProp = (id, cb) => {
