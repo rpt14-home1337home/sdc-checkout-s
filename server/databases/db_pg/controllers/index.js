6b004d1edc9b3c9a269647ea1bf8ebf2c4e83a7a
Manuel Duran <manuelduran@Manuels-MacBook-Air.local>
2019-09-04 16:19:15 -0500

    Changed variable names

--- server/databases/db_pg/controllers/index.js
+++ server/databases/db_pg/controllers/index.js
@@ -2,7 +2,7 @@ const db = require('../index.js');
 
 let insertRecord = (data, cb) => {
   data = [data.id, data.checkin, data.checkout];
-  var query = `INSERT INTO checkout(propid, checkin, checkout) VALUES ($1, $2, $3)`;
+  var query = `INSERT INTO checkout(propId, checkin, checkout) VALUES ($1, $2, $3)`;
   db.any(query, data)
     .then((res) => {
       console.log(`Inserted ${data} into table`);
@@ -15,14 +15,14 @@ let insertRecord = (data, cb) => {
 }
 
 let getRecordsByProp = (id, cb) => {
-  db.any(`SELECT * FROM checkout WHERE propid = $1`, [id])
+  db.any(`SELECT * FROM checkout WHERE propId = $1`, [id])
     .then(res => cb(null, res))
     .catch(e => cb(e));
 }
 
 const deleteRecord = (data, cb) => {
   data = [data.id, data.checkin, data.checkout];
-  var query = `DELETE from checkout where propid = $1 AND checkin = $2 AND checkout = $3`;
+  var query = `DELETE from checkout where propId = $1 AND checkin = $2 AND checkout = $3`;
   db.any(query, data)
     .then((res) => {
       console.log(`Deleted ${data} from table`);
@@ -33,7 +33,7 @@ const deleteRecord = (data, cb) => {
 
 const alterRecord = (data, cb) => {
   var data = [data.id, data.oldCheckin, data.oldCheckout, data.checkin, data.checkout];
-  var query = `update checkout set checkin = $4, checkout = $5 where prop_id = $1 AND checkin = $2 AND checkout = $3`
+  var query = `update checkout set checkin = $4, checkout = $5 where propId = $1 AND checkin = $2 AND checkout = $3`
   db.any(query, data)
     .then((res) => {
       console.log(`Altered record to ${JSON.stringify(data)}`);
